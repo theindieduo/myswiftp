fbc8b1e1cea6522e191b3b6c5b2e85a47f691e17
Anshuman patel <anshuman.patel@koshex.com>
2024-11-08 16:56:08 +0530

    first commit

---
+++ Sources/MySwiftPackage/MySwiftPackage.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+import Foundation
+
+public class MySwiftPackage {
+    public init() {}
+
+    public func sayHello() {
+        print("Hello")
+    }
+}
